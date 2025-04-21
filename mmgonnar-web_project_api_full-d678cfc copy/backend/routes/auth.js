cc41a140126f9610525fbe5286d20ef551d91779
horacio-tripleten <horacio.leon@tripleten-team.com>
2025-04-21 13:58:35 -0600

    Delete mmgonnar-web_project_api_full-d678cfc copy/backend directory

--- mmgonnar-web_project_api_full-d678cfc copy/backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { login, createUser } = require('../controllers/users');
-
-router.post('/signin', login);
-router.post('/signup', createUser);
-
-module.exports = router;
